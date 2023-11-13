15f963b0a465f6c78840988c7d30c6e91bd97865
bumstead-bumstead <dldyghks951@gmail.com>
2023-11-13 21:50:30 +0900

    test: Day - 도메인 로직 테스트 코드 구현

--- src/test/java/christmas/model/DayTest.java
+++ src/test/java/christmas/model/DayTest.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -36,5 +41,44 @@ class DayTest {
             //then
             assertThat(day1).isSameAs(day2);
         }
+    }
+
+    @DisplayName("isWeekend()를 실행했을 때, 토요일, 일요일을 제외하고 false를 반환한다.")
+    @MethodSource("provideDayAndBoolean")
+    @ParameterizedTest
+    void isWeekend(int dayInt, boolean expectedResult) {
+        //given
+        Day day = Day.of(dayInt);
+        //when
+        boolean actualResult = day.isWeekend();
+
+        //then
+        assertThat(actualResult)
+                .isEqualTo(expectedResult);
+    }
+
+    @DisplayName("isComponentOf()를 실행했을 때, 해당 리스트에 포함되는 경우 true를 반환한다.")
+    @MethodSource("provideDayAndBoolean")
+    @ParameterizedTest
+    void isComponentOf(int dayInt, boolean expectedResult) {
+        //given
+        List<Integer> list = List.of(2, 3);
+        Day day = Day.of(dayInt);
+        //when
+        boolean actualResult = day.isComponentOf(list);
+
+        //then
+        assertThat(actualResult)
+                .isEqualTo(expectedResult);
+    }
+
+    private static Stream<Arguments> provideDayAndBoolean() {
+        return Stream.of(
+                Arguments.of(1, false),
+                Arguments.of(2, true),
+                Arguments.of(3, true),
+                Arguments.of(4, false),
+                Arguments.of(5, false)
+        );
     }
 }
